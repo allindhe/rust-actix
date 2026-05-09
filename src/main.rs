9e0c182a3cf269b13c58fbad2f498b2f97f4efac
Alexander Lindhe <allindhe@gmail.com>
2026-05-08 22:25:27 -0500

    Add route Health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, get};
+
+#[get("/health_check")]
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health_check))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
